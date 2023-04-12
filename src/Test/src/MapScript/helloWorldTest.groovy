b0e11369ae615466394cf86e775967330df8eff0
Markus Schmidt <markus@markusschmidt.pro>
2023-04-12 09:40:25 +0200

    First working version

---
+++ src/Test/src/MapScript/helloWorldTest.groovy
@@ -0,0 +1,39 @@
+package MapScript
+
+
+import groovy.transform.TypeChecked
+import msPro.mgf4boomi.ExecutionUtilContexts
+import msPro.mgf4boomi.MapScript
+import org.junit.Test 
+/**
+ * TEST serialization of a DataContext object.
+ */
+@TypeChecked
+class HelloWorldTest {
+
+    //
+    // Specify Test Script and Testdata directories.
+    //
+    //final String TESTDATA_DIR = 'TestData/MSGJsonToHtml'
+    final MapScript _script = new MapScript('helloWorld.groovy')
+    
+    
+    @Test
+    void test01() {
+
+        //
+        // A Map script requires an ExecutionContext and
+        // a Map with an entry for each input parameter.
+        // The output is another Map containing all output parameters.
+        //
+        Map inputParameters = [
+                a: 5,
+                b: 7
+        ]
+        
+        Map outputParameters = _script.run( ExecutionUtilContexts.empty(), inputParameters)
+
+        println( "Test Total = " + outputParameters.total)
+        assert  outputParameters.total == inputParameters.a + inputParameters.b
+    }
+}
