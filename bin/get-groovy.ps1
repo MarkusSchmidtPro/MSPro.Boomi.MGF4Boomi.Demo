e9f1ce8a2bf4bc83a3de7c8f8e232de01b654622
MSPro <Markus@MarkusSchmidt.PRO>
2023-06-23 15:19:56 +0200

    get groovy added

---
+++ bin/get-groovy.ps1
@@ -0,0 +1,45 @@
+# ====================================================================================================
+#   Download the groovy-2.4.13 SDK from the internet
+#   install to $userhome\.groovy\sdk\v$groovyVersion
+# ====================================================================================================
+
+
+# ******** MAIN ******************
+
+$groovyVersion = "2.4.13"
+$userhome = $env:homedrive + $env:homepath
+$groovySdkPath =  "$userhome\.groovy\sdk"
+
+if( !(Test-Path $groovySdkPath) ) { 
+    New-Item -ItemType Directory -Path $groovySdkPath 
+}
+Write-Host "Installtion Groovy SDK v$groovyVersion into: $groovySdkPath"
+Get-Groovy $groovySdkPath $groovyVersion 
+
+# ******** MAIN ******************
+
+
+
+function Get-Groovy {
+    Param( [string] $sdkPath)
+    Param( [string] $groovyVersion)
+
+    $zipFileName = "apache-groovy-binary-$groovyVersion"
+    $url = "https://archive.apache.org/dist/groovy/$groovyVersion/distribution/$zipFileName.zip"
+
+    Push-Location
+    Set-Location -Path $sdkPath
+    Write-Host Get-Location
+
+    Write-Host "Downloading Groovy from $url"
+
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest $url -OutFile .\$zipFileName.zip
+    $ProgressPreference = 'Continue'
+
+    Expand-Archive .\$zipFileName.zip .\
+    Remove-Item .\$zipFileName.zip
+    Pop-Location
+}
+
+
